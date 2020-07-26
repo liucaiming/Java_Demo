5d48850d6e293be2fc023e70b1d01041d0b61aa4
liujunguo <liujunguo2012@126.com>
2020-07-26 15:49:05 +0800

    init

---
+++ springboot-demo/src/main/java/com/lcm/it/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.lcm.it.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+
+    @GetMapping("index")
+    public String index(){
+        return "SUCESS";
+    }
+}
